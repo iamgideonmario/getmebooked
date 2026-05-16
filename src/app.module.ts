f87ccee9faed2bd88c269be1f3cfdf5d7c380123
Gideon <iamgideonmario@gmail.com>
2026-05-16 12:46:41 -0400

    fix app.module syntax final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AuthController } from './auth/auth.controller';import { AppController } from './app.controller';
+import { AppController } from './app.controller';
+
+import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 import { BusinessController } from './business/business.controller';
@@ -9,24 +11,22 @@ import { BusinessService } from './business/business.service';
 import { BookingController } from './booking/booking.controller';
 import { BookingService } from './booking/booking.service';
 
-
 import { PaymentController } from './payment/payment.controller';
 import { PaymentService } from './payment/payment.service';
+
 @Module({
   controllers: [
     AppController,
     AuthController,
     BusinessController,
-    BookingController
+    BookingController,
+    PaymentController
   ],
-
-  controllers: [..., PaymentController],controllers: [...,: [..., PaymentService],
-
   providers: [
     AuthService,
     BusinessService,
-    BookingService
+    BookingService,
+    PaymentService
   ],
 })
 export class AppModule {}
-
