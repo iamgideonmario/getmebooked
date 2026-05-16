1b7020326a9539c60729f3f921832bcff6976358
Gideon <iamgideonmario@gmail.com>
2026-05-16 13:38:46 -0400

    add booking page route properly

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -12,7 +12,9 @@ import { BusinessService } from './business.service';
 import { AuthGuard } from '../auth/guards/auth.guard';
 
 import { Request, Response } from 'express';
-
+import { Res } from '@nestjs/common';
+import { Response } from 'express';
+import { prisma } from '../prisma/prisma.service';
 @Controller('business')
 export class BusinessController {
   constructor(private businessService: BusinessService) {}
@@ -36,4 +38,13 @@ export class BusinessController {
       businesses,
     });
   }
+
+  @Get()
+async bookingPage(@Res() res: Response) {
+  const services = await prisma.service.findMany();
+
+  return res.render('booking/index', {
+    services,
+  });
+}
 }
